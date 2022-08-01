dc5fc56fe809fb73ab44a232e5ad03de6d75de6f
Chreyd <chreydmapeba@gmail.com>
2022-08-02 00:42:14 +0100

    ActivityIndicator

---
+++ App.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { View, Text, Image, ScrollView, TextInput, Button, FlatList, StyleSheet, SectionList, ActivityIndicator } from 'react-native';
+import { Box } from 'react-native-flex-layout';
+import { Flex, Spacer } from 'react-native-flex-layout';
+import { Stack, HStack, VStack } from 'react-native-flex-layout';
+import { Wrap } from 'react-native-flex-layout';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*                  
+              ActivityIndicator
+
+const App = () =>{
+  return(
+    <View style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator/>
+      <ActivityIndicator size= "large"/>
+      <ActivityIndicator size="small" color="#0000ff" hidesWhenStopped ="false"/>
+      <ActivityIndicator size="large" color="00ff00" animating='true'/>
+    </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+});
+
+*/
+export default App;
