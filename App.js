ac2e08044f56d1be77146089ee36b97443ed6a09
Chreyd <chreydmapeba@gmail.com>
2022-08-05 01:16:43 +0100

    ImageBackground

--- App.js
+++ App.js
@@ -1,46 +1,37 @@
 import React, { useState } from "react";
-import { View, Text, Image, ScrollView, TextInput, Button, FlatList, StyleSheet, SectionList, ActivityIndicator, SafeAreaView, Alert, StatusBar, TouchableOpacity   } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, Button, FlatList, StyleSheet, SectionList, ActivityIndicator, SafeAreaView, Alert, StatusBar, TouchableOpacity, ImageBackground   } from 'react-native';
 import { Box } from 'react-native-flex-layout';
 import { Flex, Spacer } from 'react-native-flex-layout';
 import { Stack, HStack, VStack } from 'react-native-flex-layout';
 import { Wrap } from 'react-native-flex-layout';
 
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
+const App = () =>(
+  <View style={styles.container}>
+    <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside</Text>
+    </ImageBackground>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container:{
-    paddding: 50,
+    flex: 1,
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+  image:{
+    flex: 1,
+    justifyContent: "center",
   },
-  logo:{
-    width: 66,
-    height: 58
+  text:{
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   }
 });
-
-const App = () =>{
-  return(
-    <View style={styles.container}>
-{/*       <Image
-        style={styles.tinyLogo}
-        source={require('assets/telechargement.jpg')}
-      /> */}
-      <Image
-        style={styles.tinyLogo}
-        source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-      />
-      <Image
-        style={styles.logo}
-        source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
-      />
-    </View>
-  )
-}
-
-
-
 
 
 
